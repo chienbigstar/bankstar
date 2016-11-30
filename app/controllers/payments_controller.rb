13b5bde1260cbf60129358daa35ccede5702f18e
chienbigstar <chienbigstar@gmail.com>
2016-11-30 10:37:37 +0700

    gggggg

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,11 +10,20 @@ class PaymentsController < ApplicationController
     @order = Order.find_by_id payment_params[:order_id]
     new_money = current_user.money.to_i - @order.money.to_i
     current_user.update_attribute :money, new_money
+    reply_link
     render html: :ok
   end
 
   private
   def payment_params
     params.require(:payment).permit :order_id
+  end
+
+  def reply_link
+    uri = URI('http://localhost:3000/payments/update')
+    params = {token: @order.private_token}
+    uri.query = URI.encode_www_form(params)
+    res = Net::HTTP.get_response(uri)
+    # puts res.body if res.is_a?(Net::HTTPSuccess)
   end
 end
