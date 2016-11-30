0230285f1d0d6a6f16243ba33bfe02576b5dbedd
chienbigstar <chienbigstar@gmail.com>
2016-11-30 22:00:32 +0700

    nbnb

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,3 +1,6 @@
+require "net/http"
+require "net/https"
+
 class PaymentsController < ApplicationController
   before_action :authenticate_user!
   def new
@@ -10,7 +13,7 @@ class PaymentsController < ApplicationController
     @order = Order.find_by_id payment_params[:order_id]
     new_money = current_user.money.to_i - @order.money.to_i
     current_user.update_attribute :money, new_money
-    # reply_link
+    reply_link
     render html: :ok
   end
 
